6216e45382a705a8bc8654dbe8874881999dfcb2
Sameer Raj <sameerraj0820@gmail.com>
2024-07-26 21:08:23 +0530

    add cloudinary and multer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+    path: "./env",
+});
 
 connectDB()
-.then( () => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(` Server is running at port : ${process.env.PORT}`);
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(` Server is running at port : ${process.env.PORT}`);
+        });
     })
-})
-.catch((err) => {
-    console.log("MONDO db connection failed !!!", err);
-})
-
-
-
-
+    .catch((err) => {
+        console.log("MONDO db connection failed !!!", err);
+    });
 
 // import express from "express";
 // const app = express()
