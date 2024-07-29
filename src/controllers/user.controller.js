be46d2b6d7ada22ddd2ed9a86715b510e9bdf4c1
Sameer Raj <sameerraj0820@gmail.com>
2024-07-29 20:43:28 +0530

    reduce code by using optional chaining

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -35,12 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
-    let coverImageLocalPath;
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
-        coverImageLocalPath = req.files.coverImage[0].path;
-    }
+    const coverImageLocalPath = req?.files?.coverImage?.[0]?.path;
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
