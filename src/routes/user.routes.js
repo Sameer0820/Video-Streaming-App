8d56de50a9cc68dc0cfea918b7504df595583d99
Sameer Raj <sameerraj0820@gmail.com>
2024-07-31 22:55:57 +0530

    Add user account management endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {
+    changeCurrentPassword,
+    getCurrentUser,
     loginUser,
     logOutUser,
     refreshAccessToken,
     registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -26,5 +31,15 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 export default router;
