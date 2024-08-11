ccb1085204bed0eae087ed4a4428feea7e52343d
Sameer Raj <sameerraj0820@gmail.com>
2024-08-11 11:40:29 +0530

    changed health check message

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-    return res.status(200).json(new ApiResponse(200, {}, "Health OK"));
+    return res.status(200).json(new ApiResponse(200, "OK", "Health check passed"));
 });
 
 export { healthcheck };
