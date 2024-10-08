f1788ffe3c78351f5c89d400369e4f4ace7e017f
Sameer Raj <sameerraj0820@gmail.com>
2024-10-08 23:14:40 +0530

    update multer

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -2,7 +2,11 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp");
+        const dir = './public/temp';
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null, dir);
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
