3c8aab30f7d637622e9600a4fc73f87910a33106
Sameer Raj <sameerraj0820@gmail.com>
2024-10-08 22:28:47 +0530

    update limit

--- server/src/app.js
+++ server/src/app.js
@@ -14,8 +14,8 @@ app.use(
     })
 );
 
-app.use(express.json({ limit: "1mb" }));
-app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
