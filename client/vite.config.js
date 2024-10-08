0bae535fc5bbd85ea82b99ae21cf3702de832a5f
Sameer Raj <sameerraj0820@gmail.com>
2024-10-08 23:35:22 +0530

    update proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     server: {
         proxy: {
-            "/api": "https://streamify-rose-six.vercel.app",
+            "/api": "https://streamify-rose-six.vercel.app/api/v1",
         },
     },
     plugins: [react()],
--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -3,11 +3,7 @@ import fs from 'fs';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const dir = './public/temp';
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir, { recursive: true });
-        }
-        cb(null, dir);
+        cb(null, "./public/temp");
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
